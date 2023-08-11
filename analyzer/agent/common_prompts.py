fb38e3c0a8bec120b4f3397b973f5bb4ba4aee93
TristeMq <glebpaninpt@gmail.com>
2023-08-11 11:06:23 +0300

    better prompts

--- analyzer/agent/common_prompts.py
+++ analyzer/agent/common_prompts.py
@@ -69,10 +69,12 @@ Action: the action you take. It's one of {tool_names}. You have to write "Action
 Action Input: the input to the action.
 AResult: the result of the action.
 Final Result: the final result of the task. Write what you did, be reasonably detailed and include names of plot files.
-FIRSTLY, if you need to find something in the table, LOOK FOR COLUMNS THAT ARE MOST RELATED TO YOUR VARIABLE and
+FIRSTLY, Think about what you were asked about, don't cling to the keywords right away. Think about what variables we will be talking about.
+If you need to find something in the table, LOOK FOR COLUMNS THAT ARE MOST RELATED TO YOUR VARIABLE and
 Try to find your search object or similar ones (in plural and single form or swap words for example) using 'difflib.SequenceMatcher()' or 'difflib.get_close_mathces' and 
 use 'str.contains' function for searching required rows (use different forms of the words when searching so as not to lose the necessary data)
 ALWAYS USE BOTH OF THEM TO FIND FULL INFORMATION OR THE WORLD WILL BE DESTROYED
+IF VALUE THAT YOU ARE ASKED TO FIND IS EQUAL TO 0, TRY TO FIND IN ANOTHER COLUMNS AND BE VERY CAREFUL
 Example of your actions:  
 I need to check the dataframe to see which rows where 'Материал Имя' or 'Материал имя (полное)1' contains the information about 'Керамические шары':
 # Define the target variable
@@ -109,7 +111,7 @@ Your task is to provide an answer to a question in user-friendly form, understan
 You should handle units of measure properly, considering relationships between them. Take into account, that 1 ton contains 7.28 barrels.
 When counting value, report about its units of measure using comments.
 When looking for something in the table, do not forget to look for different forms necessarily
-IT IS FORBIDDEN TO HALLUCINATE NUMBERS. YOU CAN ONLY USE DATA PROVIDED IN THE TABLE AND MAKE CONCLUSIONS BASED ON IT, GAINED BY python_repl_ast tool.
+IT IS FORBIDDEN TO HALLUCINATE NUMBERS. BE CAREFUL 8.500 IN SOME CASES CAN BE 8500, YOU SHOULD UNDERSTAND IT FROM YOUR DATA. YOU CAN ONLY USE DATA PROVIDED IN THE TABLE AND MAKE CONCLUSIONS BASED ON IT, GAINED BY python_repl_ast tool.
 Answer should be in the form of analysis, not just data. Don't use names of columns in answer. Instead of that, describe them.
 There is a lot of missing values in table. Handle them properly, take them into account while analyzing.
 Do not put variables in your answer, only numbers
